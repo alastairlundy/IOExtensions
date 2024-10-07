cb3a66347ed05c6a89440e17fb8911526c276652
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-10-07 18:03:57 +0100

    add FileAppender

---
+++ AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO/Files/Concatenation/FileAppender.cs
@@ -0,0 +1,216 @@
+﻿/*
+
+    BasisBox - ConCat Library
+    Copyright (C) 2024 Alastair Lundy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using AlastairLundy.Extensions.IO.Files.Abstractions;
+using AlastairLundy.Extensions.IO.Localizations;
+using NLine.Library;
+using NLine.Library.Abstractions;
+
+// ReSharper disable RedundantIfElseBlock
+
+namespace AlastairLundy.Extensions.IO.Files.Concatenation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FileAppender
+    {
+        protected List<string> AppendedFileContents;
+    
+        public bool AddLineNumbers { get; protected set; }
+    
+        protected IFileFinder fileFinder;
+        protected ILineNumberer lineNumberer;
+    
+        public FileAppender()
+        {
+            AppendedFileContents = new List<string>();
+            AddLineNumbers = false;
+            fileFinder = new FileFinder();
+            lineNumberer = new LineNumberer();
+        }
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="addLineNumbers">Adds line numbers to the appended file contents.</param>
+        public FileAppender(bool addLineNumbers)
+        {
+            this.fileFinder = new FileFinder();
+            this.lineNumberer = new LineNumberer();
+            AppendedFileContents = new List<string>();
+            AddLineNumbers = addLineNumbers;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileFinder"></param>
+        /// <param name="addLineNumbers">Adds line numbers to the appended file contents.</param>
+        public FileAppender(bool addLineNumbers, IFileFinder fileFinder, ILineNumberer lineNumberer)
+        {
+            this.fileFinder = fileFinder;
+            this.lineNumberer = lineNumberer;
+            AppendedFileContents = new List<string>();
+            AddLineNumbers = addLineNumbers;
+        }
+    
+        /// <summary>
+        /// Attempts to append the contents of a file to an existing list.
+        /// </summary>
+        /// <param name="fileToBeAppended">The file to have its contents appended to the existing file contents. If no existing file contents exists, this will become the contents appended to in the future.</param>
+        /// <returns>true if the files where successfully appended; returns false otherwise.</returns>
+        public bool TryAppendFile(string fileToBeAppended)
+        {
+            try
+            {
+                AppendFile(fileToBeAppended);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    
+        /// <summary>
+        /// Appends the contents of a file to an existing list.
+        /// </summary>
+        /// <param name="fileToBeAppended">The file to be appended.</param>
+        /// <exception cref="Exception">Thrown if the file appending fails.</exception>
+        /// <exception cref="FileNotFoundException">Thrown if the file specified is not found.</exception>
+        public void AppendFile(string fileToBeAppended)
+        {
+            if (fileFinder.IsAFile(fileToBeAppended) || File.Exists(fileToBeAppended))
+            {
+                try
+                {
+                    string[] fileContents = File.ReadAllLines(fileToBeAppended);
+
+                    AppendFileContents(fileContents);
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(exception.Message, exception);
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException(Resources.Exceptions_FileNotFound, fileToBeAppended);
+            }
+        }
+
+        /// <summary>
+        /// Append the contents of an ordered enumerable of files to an existing list.
+        /// </summary>
+        /// <param name="filesToBeAppended">The files to be appended to the existing appended content.</param>
+        public void AppendFiles(IOrderedEnumerable<string> filesToBeAppended)
+        {
+            AppendFiles(filesToBeAppended.ToArray());
+        }
+
+        /// <summary>
+        /// Append the contents of the IEnumerable of files to an existing list.
+        /// </summary>
+        /// <param name="filesToBeAppended">The files to be appended to the existing appended content.</param>
+        public void AppendFiles(IEnumerable<string> filesToBeAppended)
+        {
+            foreach (string file in filesToBeAppended)
+            {
+                AppendFile(file);
+            }
+        }
+
+        /// <summary>
+        /// Append the contents of a file to an existing list.
+        /// </summary>
+        /// <param name="fileContents">The file contents to be appended.</param>
+        /// <exception cref="Exception">Thrown if the file appending fails.</exception>
+        public void AppendFileContents(IEnumerable<string> fileContents)
+        {
+            try
+            {
+                string[] fileContentsArray = fileContents as string[] ?? fileContents.ToArray();
+            
+                if (AddLineNumbers)
+                {
+                    fileContentsArray = lineNumberer.AddLineNumbers(fileContentsArray, ". ").ToArray();
+                }
+
+                foreach (string str in fileContentsArray)
+                {
+                    AppendedFileContents.Add(str);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message, exception);
+            }
+        }
+
+        /// <summary>
+        /// Returns the appended contents to an IEnumerable.
+        /// </summary>
+        /// <returns>the list of appended strings as an enumerable.</returns>
+        public IEnumerable<string> ToEnumerable()
+        {
+            return AppendedFileContents;
+        }
+
+        /// <summary>
+        /// Writes the appended strings to a file.
+        /// </summary>
+        /// <param name="filePath">The path to save the file to.</param>
+        /// <exception cref="ArgumentException">Thrown if an invalid file path is provided.</exception>
+        /// <exception cref="FileNotFoundException">Thrown if the file specified is not found.</exception>
+        /// <exception cref="Exception">Thrown if an exception occurs when attempting to write the file.</exception>
+        public void WriteToFile(string filePath)
+        {
+            if (fileFinder.IsAFile(filePath))
+            {
+                try
+                {
+                    if (File.Exists(filePath))
+                    {
+                        File.Delete(filePath);
+                    }
+
+                    File.WriteAllLines(filePath, AppendedFileContents);
+                }
+                catch (FileNotFoundException exception)
+                {
+                    throw new FileNotFoundException(exception.Message, filePath, exception);
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(exception.Message, exception);
+                }
+            }
+            else
+            {
+                throw new ArgumentException(Resources.Exceptions_NoFileProvided, filePath);
+            }
+        }
+    }
+}
