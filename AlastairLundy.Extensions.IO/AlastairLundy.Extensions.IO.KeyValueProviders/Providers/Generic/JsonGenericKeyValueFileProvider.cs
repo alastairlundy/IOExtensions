7512ee64854f416c41405cdabc74f298b8d0b8f9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-10-22 17:12:24 +0100

    disable IO KeyValueProvider extensions for now

--- AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO.KeyValueProviders/Providers/Generic/JsonGenericKeyValueFileProvider.cs
+++ AlastairLundy.Extensions.IO/AlastairLundy.Extensions.IO.KeyValueProviders/Providers/Generic/JsonGenericKeyValueFileProvider.cs
@@ -10,41 +10,43 @@ namespace AlastairLundy.Extensions.IO.KeyValueProviders.Providers.Generic
     {
         public KeyValuePair<TKey, TValue>[] Get(string pathToFile)
         {
-            string json = File.ReadAllText(pathToFile);
-
-            if (json.Contains("key:"))
-            {
-                json = json.Replace("key:", "Key:");
-            }
-
-            if (json.Contains("value:"))
-            {
-                json = json.Replace("value:", "Value:");
-            }
-
-            KeyValuePair<TKey, TValue>[] data  = JsonSerializer.Deserialize<>(json);
-
-            return data;
+            // string json = File.ReadAllText(pathToFile);
+            //
+            // if (json.Contains("key:"))
+            // {
+            //     json = json.Replace("key:", "Key:");
+            // }
+            //
+            // if (json.Contains("value:"))
+            // {
+            //     json = json.Replace("value:", "Value:");
+            // }
+            //
+            // KeyValuePair<TKey, TValue>[] data  = JsonSerializer.Deserialize<>(json);
+            //
+            // return data;
+            throw new NotImplementedException();
         }
 
         public void WriteToFile(KeyValuePair<TKey, TValue>[] data, string pathToFile)
         {
-            try
-            {
-                StringBuilder stringBuilder = new StringBuilder();
-
-                foreach (KeyValuePair<TKey, TValue> keyValuePair in data)
-                {
-                    JsonElement jsonElement = JsonElement.
-                }
-
-                File.WriteAllText(pathToFile, contents);
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception.ToString());
-                throw;
-            }
+            throw new NotImplementedException();
+            // try
+            // {
+            //     StringBuilder stringBuilder = new StringBuilder();
+            //
+            //     foreach (KeyValuePair<TKey, TValue> keyValuePair in data)
+            //     {
+            //         JsonElement jsonElement = JsonElement.
+            //     }
+            //
+            //     File.WriteAllText(pathToFile, contents);
+            // }
+            // catch (Exception exception)
+            // {
+            //     Console.WriteLine(exception.ToString());
+            //     throw;
+            // }
         }
     }
 }
